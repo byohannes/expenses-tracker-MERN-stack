022358c7c0af5d79c4028bde6fbcbd95c0f7fae5
byohannes <byohannes44@gmail.com>
2020-07-31 00:55:49 +0100

    added useeffect for get transactions function

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Transaction } from './Transaction';
 
 import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+        {transactions.map(transaction => (<Transaction key={transaction._id} transaction={transaction} />))}
       </ul>
     </>
   )
